0835d484db1cfefbd63f67e467c1f2c631b48750
Mohamatam <102506744+Mohamatam@users.noreply.github.com>
2022-05-06 20:36:27 +0530

    Add files via upload

---
+++ ADO Assignment2/BusInformation.sql
@@ -0,0 +1,52 @@
+use newdb
+create table BusInformation (
+
+BusID int Primary Key,
+
+BoardingPoint nvarchar(50),
+
+TravelDate datetime,
+
+Amount decimal(10,2),
+
+Rating int
+
+)
+
+Alter table BusInformation
+add DroppingPoint nvarchar(50)
+
+create procedure Insert_BusInfo (
+	@BusID int,
+	@BoardingPoint nvarchar(50), 
+	@TravelDate datetime, 
+	@Amount decimal(10,2), 
+	@Rating int,
+	@DroppingPoint nvarchar(50)
+	)
+as 
+Begin 
+  Insert into BusInformation values (@BusID, @BoardingPoint, @TravelDate, @Amount, @Rating, @DroppingPoint )
+End
+
+-- Executing Procedure
+exec Insert_BusInfo
+1,	'BGL',	'20170618',	400.65,	2, 'HYD'
+
+exec Insert_BusInfo
+2,	'HYD',	'20171005',	600.00,	3, 'CHN'
+
+exec Insert_BusInfo
+3,	'CHN',	'20160725',	445.95,	3, 'PUN'
+
+exec Insert_BusInfo
+4,	'PUN',	'20171210',	543.00,	4, 'MUM'
+
+exec Insert_BusInfo
+5,	'MUM',	'20170128',	500.50,	4, 'PUN'
+
+exec Insert_BusInfo
+6,	'PUN',	'20160327',	333.55,	3, 'MUM'
+
+exec Insert_BusInfo
+7,	'MUM',	'20161106',	510.00,	4, 'BGL'
---
+++ ADO Assignment2/WebForm1.aspx.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Ado_Assignment_2._1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String connectStr = ConfigurationManager.ConnectionStrings["conn"].ConnectionString;
+
+            Bus1(connectStr);
+
+            Bus2(connectStr);
+
+            Bus3(connectStr);
+        }
+
+        public void Bus1(String connectStr)
+        {
+            using (SqlConnection conn = new SqlConnection(connectStr))
+            {
+
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("Select BoardingPoint, TravelDate from BusInformation where Amount > 450", conn);
+                SqlDataReader datareader = cmd.ExecuteReader();
+
+                GridView1.DataSource = datareader;
+                GridView1.DataBind();
+            }
+        }
+
+        public void Bus2(String connectStr)
+        {
+            using (SqlConnection conn = new SqlConnection(connectStr))
+            {
+
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("Select BusID, DroppingPoint from BusInformation where TravelDate = '2017-12-10'", conn);
+                SqlDataReader datareader = cmd.ExecuteReader();
+
+                GridView2.DataSource = datareader;
+                GridView2.DataBind();
+
+            }
+        }
+        public void Bus3(String connectStr)
+        {
+            using (SqlConnection conn = new SqlConnection(connectStr))
+            {
+
+                conn.Open();
+
+                SqlCommand cmd = new SqlCommand("Select BusID, DroppingPoint, Rating from BusInformation where Rating > 3", conn);
+                SqlDataReader datareader = cmd.ExecuteReader();
+
+                GridView3.DataSource = datareader;
+                GridView3.DataBind();
+            }
+        }
+    }
+}
